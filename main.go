0b6208a008d1e9db2f7892a35c17dc8eb1ae82a0
Brett Johnson <brejohns@redhat.com>
2024-05-30 14:06:57 +1000

    update main.go

--- main.go
+++ main.go
@@ -9,35 +9,9 @@ import (
 
 func main() {
 
-	twoDigitList := combinations.GetCombinations(1, nil)
-	threeDigitList := combinations.GetCombinations(2, twoDigitList)
-	fourDigitList := combinations.GetCombinations(3, threeDigitList)
-	fiveDigitList := combinations.GetCombinations(4, fourDigitList)
-	sixDigitList := combinations.GetCombinations(5, fiveDigitList)
-	sevenDigitList := combinations.GetCombinations(6, sixDigitList)
-	eightDigitList := combinations.GetCombinations(7, sevenDigitList)
-	nineDigitList := combinations.GetCombinations(8, eightDigitList)
+	dataSet := combinations.GenerateDataSet()
 
-	twoDigitCombinations := combinations.GetValues(twoDigitList)
-	threeDigitCombinations := combinations.GetValues(threeDigitList)
-	fourDigitCombinations := combinations.GetValues(fourDigitList)
-	fiveDigitCombinations := combinations.GetValues(fiveDigitList)
-	sixDigitCombinations := combinations.GetValues(twoDigitList)
-	sevenDigitCombinations := combinations.GetValues(sixDigitList)
-	eightDigitCombinations := combinations.GetValues(eightDigitList)
-	nineDigitCombinations := combinations.GetValues(nineDigitList)
-
-	c := combinations.Combinations{}
-	c.Add("2", twoDigitCombinations)
-	c.Add("3", threeDigitCombinations)
-	c.Add("4", fourDigitCombinations)
-	c.Add("5", fiveDigitCombinations)
-	c.Add("6", sixDigitCombinations)
-	c.Add("7", sevenDigitCombinations)
-	c.Add("8", eightDigitCombinations)
-	c.Add("9", nineDigitCombinations)
-
-	jsonString, _ := json.Marshal(c)
+	jsonString, _ := json.Marshal(dataSet)
 
 	fmt.Printf("%s", jsonString)
 }
