562317992c6eaba6061021ebaec09d3fc4981305
Brett Johnson <brejohns@redhat.com>
2024-05-31 17:31:27 +1000

    add string to int conversion

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -10,6 +10,14 @@ func IntToString(i int) string {
 	return strconv.Itoa(i)
 }
 
+func StringToInt(i string) (int, error) {
+	x, err := strconv.Atoi(i)
+	if err != nil {
+		return x, err
+	}
+	return x, nil
+}
+
 func SortString(w string) string {
 	s := strings.Split(w, "")
 	sort.Strings(s)
