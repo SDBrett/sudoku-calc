3aa42afb779a1be1b489fa6aa0c694fd3f2d6e88
Brett Johnson <brejohns@redhat.com>
2024-06-01 15:33:48 +1000

    reorganize file structure

--- pkg/combinations.go
+++ pkg/combinations.go
@@ -1,4 +1,4 @@
-package combinations
+package sudokucalc
 
 import (
 	"log"
@@ -22,34 +22,7 @@ var all = NumberList{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
 type NumberList []string
 type ValueErr string
 type ValueCombinations map[int]NumberList
-type DataSet map[int]ValueCombinations
-type ValidateCombinations map[string]bool
-
-type DataSetQuery struct {
-	NumberOfDigits   int        //The number of digits
-	Value            int        //The Value for the digits to sum to
-	NumbersToExclude NumberList //Numbers which cannot be part of the solution
-	NumbersToInclude NumberList //Numbers which must be part of the solution
-}
-
-// Generate the DataSet of all possible combinations
-func GenerateDataSet() DataSet {
-	combinationsByValue := getCombinationsForValue()
-
-	// Initialize dataSet
-	dataSet := newDataSet()
-
-	// k is the sum of the items in the array
-	// v is the list of combinations
-	for k, v := range combinationsByValue {
-		for i := 0; i < len(v); i++ {
-			// v[i] is an individual combination for total k
-			dataSet[len(v[i])].Update(k, v[i])
-		}
-	}
-
-	return dataSet
-}
+type ValidCombinations map[string]bool
 
 // Generates a list of valid number combinations
 func generateNumberLists(idx int, nl NumberList) NumberList {
@@ -115,9 +88,9 @@ func getCombinationsForValue() ValueCombinations {
 
 // Look up number combinations for a given value
 // Returns list of combinations for that value
-func (dc ValueCombinations) Search(value int) (NumberList, error) {
+func (vc ValueCombinations) Search(value int) (NumberList, error) {
 
-	nl, ok := dc[value]
+	nl, ok := vc[value]
 
 	if !ok {
 		return nil, ErrNotFound
@@ -126,16 +99,16 @@ func (dc ValueCombinations) Search(value int) (NumberList, error) {
 }
 
 // Update the combinations to a list of combinations for a a given value
-func (dc ValueCombinations) Update(value int, combination string) error {
+func (vc ValueCombinations) Update(value int, combination string) error {
 
-	nl, err := dc.Search(value)
+	nl, err := vc.Search(value)
 	switch err {
 	case ErrNotFound:
 		nl := NumberList{combination}
-		dc[value] = nl
+		vc[value] = nl
 	case nil:
 		nl = append(nl, combination)
-		dc[value] = nl
+		vc[value] = nl
 	default:
 		return err
 	}
@@ -147,33 +120,9 @@ func (e ValueErr) Error() string {
 	return string(e)
 }
 
-// Returns possible values and their combinations with a given
-// number of digits.
-func (dc DataSet) Search(value int) (ValueCombinations, error) {
-
-	nl, ok := dc[value]
-
-	if !ok {
-		return nil, ErrNotFound
-	}
-	return nl, nil
-}
-
-func (dc DataSet) Query(dsq DataSetQuery) (NumberList, error) {
-
-	nl := NumberList{}
-
-	err := dsq.Validate()
-	if err != nil {
-		return nil, err
-	}
-
-	return nl, nil
-}
-
 func GetValidCombinations(combinations, exclude, include NumberList) NumberList {
 
-	valid := ValidateCombinations{}
+	valid := ValidCombinations{}
 	nl := NumberList{}
 
 	for _, item := range combinations {
@@ -199,7 +148,7 @@ func GetValidCombinations(combinations, exclude, include NumberList) NumberList 
 
 // Determine if combination is valid. set exclusion to true if combination should not contain digits in NumberList
 // Set exclude to false if combination must contain numbers from number list
-func (v ValidateCombinations) CheckIfValid(candidateCombination string, nl NumberList, exclusion bool) {
+func (v ValidCombinations) CheckIfValid(candidateCombination string, nl NumberList, exclusion bool) {
 
 	switch exclusion {
 	case true:
@@ -217,59 +166,4 @@ func (v ValidateCombinations) CheckIfValid(candidateCombination string, nl Numbe
 			}
 		}
 	}
-}
-
-func (dsq DataSetQuery) Validate() error {
-
-	var err error
-	var x int
-
-	err = utils.ValidateNumberRange(1, 9, dsq.NumberOfDigits)
-	if err != nil {
-		return err
-	}
-
-	err = utils.ValidateNumberRange(3, 45, dsq.Value)
-	if err != nil {
-		return err
-	}
-
-	for _, v := range dsq.NumbersToInclude {
-		x, err = utils.StringToInt(v)
-		if err != nil {
-			return err
-		}
-		err = utils.ValidateNumberRange(1, 9, x)
-		if err != nil {
-			return err
-		}
-	}
-
-	for _, v := range dsq.NumbersToExclude {
-		x, err = utils.StringToInt(v)
-		if err != nil {
-			return err
-		}
-		err = utils.ValidateNumberRange(1, 9, x)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func newDataSet() DataSet {
-	dataSet := DataSet{
-		2: ValueCombinations{},
-		3: ValueCombinations{},
-		4: ValueCombinations{},
-		5: ValueCombinations{},
-		6: ValueCombinations{},
-		7: ValueCombinations{},
-		8: ValueCombinations{},
-		9: ValueCombinations{},
-	}
-
-	return dataSet
 }
