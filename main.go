c497994494c66e215d9250c05885e75df211667e
Brett Johnson <brett@sdbrett.com>
2024-07-10 16:46:23 +1000

    add graceful shutdown

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	sudokucalc "github.com/sdbrett/sudoku-calc/pkg"
@@ -32,9 +38,36 @@ func queryDataSet(c *gin.Context) {
 
 func main() {
 
+	gracefulShutdown := make(chan os.Signal, 1)
+	signal.Notify(gracefulShutdown, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
 	r := gin.Default()
 	r.GET("/dataset", getDataset)
 	r.POST("/dataset", queryDataSet)
-	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r.Handler(),
+	}
+
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	<-gracefulShutdown
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	// catching ctx.Done(). timeout of 5 seconds.
+	select {
+	case <-ctx.Done():
+		log.Println("timeout of 5 seconds.")
+	}
+	log.Println("Server exiting")
 }
