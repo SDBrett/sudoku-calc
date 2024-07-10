7577d9b48c81baae3dfdb2308e5a493cbabd8735
Brett Johnson <brett@sdbrett.com>
2024-07-10 16:46:23 +1000

    test api implementation

--- main.go
+++ main.go
@@ -1,17 +1,40 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	sudokucalc "github.com/sdbrett/sudoku-calc/pkg"
 )
 
+var ds = sudokucalc.GenerateDataSet()
+
+// getAlbums responds with the list of all albums as JSON.
+func getDataset(c *gin.Context) {
+
+	c.IndentedJSON(http.StatusOK, ds)
+}
+
+func queryDataSet(c *gin.Context) {
+	var dsq sudokucalc.DataSetQuery
+	var err error
+	if err = c.BindJSON(&dsq); err != nil {
+		return
+	}
+
+	response, err := ds.Query(dsq)
+	if err != nil {
+		return
+	}
+
+	c.IndentedJSON(http.StatusAccepted, response)
+}
+
 func main() {
 
-	dataSet := sudokucalc.GenerateDataSet()
-
-	jsonString, _ := json.Marshal(dataSet)
-
-	fmt.Printf("%s", jsonString)
+	r := gin.Default()
+	r.GET("/dataset", getDataset)
+	r.POST("/dataset", queryDataSet)
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(":8080")
 }
