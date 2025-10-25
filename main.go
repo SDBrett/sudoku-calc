def41e576f20c8972e1d5c545f33fcf23ba4a108
Brett Johnson <brejohns@redhat.com>
2025-10-26 10:43:37 +1100

    cursor: add CALC_PORT environment variable support for HTTP port configuration

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	sudokucalc "github.com/sdbrett/sudoku-calc/pkg"
@@ -33,24 +35,34 @@ func queryDataSet(c *gin.Context) {
 }
 
 func main() {
+	// Get port from environment variable, default to 8080
+	port := os.Getenv("CALC_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	// Validate port is a valid number
+	if _, err := strconv.Atoi(port); err != nil {
+		port = "8080"
+	}
 
 	r := gin.Default()
-	
+
 	// Serve static files (CSS, JS)
 	r.Static("/static", "./static")
-	
+
 	// Load HTML templates
 	r.LoadHTMLGlob("templates/*")
-	
+
 	// Serve the main web interface
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
-	
+
 	// API endpoints
 	r.GET("/dataset", getDataset)
 	r.POST("/dataset", queryDataSet)
-	
-	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+
+	// Listen and Server on the configured port
+	r.Run(":" + port)
 }
